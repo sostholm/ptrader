a7ce2912b2793b6b75cb15f630927e7cc9047311
Samuel Ostholm <sostholm@gmail.com>
2020-10-18 18:50:24 +0200

    adding public files for frontend

--- backend/src/web_push.py
+++ backend/src/web_push.py
@@ -7,14 +7,14 @@ from pywebpush import webpush, WebPushException
 VAPID_PRIVATE_KEY = None
 VAPID_PUBLIC_KEY = None
 
-try:
-    VAPID_PRIVATE_KEY = open('private_key.txt', "r+").readline().strip("\n")
-except:
+if 'VAPID_PKEY' in os.environ:
+    VAPID_PRIVATE_KEY = os.environ["VAPID_PKEY"]
+else:
     VAPID_PRIVATE_KEY = open('/run/secrets/vapid_key_txt', "r+").readline().strip("\n")
 
-try:
-    VAPID_PUBLIC_KEY = open('public_key.txt', "r+").read().strip("\n")
-except:
+if 'VAPID_PUBLIC_KEY' in os.environ:
+    VAPID_PRIVATE_KEY = os.environ["VAPID_PUBLIC_KEY"]
+else:
     VAPID_PUBLIC_KEY = open('/run/secrets/vapid_public_key_txt', "r+").read().strip("\n")
 
 VAPID_CLAIMS = {
